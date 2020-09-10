7bb0e6e8256aa8a90492df406a33208ebcf58b35
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-10 16:01:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(speak)
   if (speak == speak.upcase) == flase
-    return "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   elsif speak == speak.upcase 
     return "NO, NOT SINCE 1938!"
   else "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
